9f3483f9fc346877bfb5dd768e5fa965e11609a4
berkde <berkdelibalta@gmail.com>
2025-07-19 19:47:26 -0400

    Updated the frontend CORS rules

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://testmind-frontend.onrender.com"],
+    allow_origins=["https://testmind-u11n.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
